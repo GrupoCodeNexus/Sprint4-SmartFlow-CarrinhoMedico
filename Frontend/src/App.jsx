c3425ea111c25dcb89374ad5541f49b3eaf4ba90
Kayque <kayquecarvalho020@gmail.com>
2025-06-09 16:32:11 -0300

    Feat: Criando rotas

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,32 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import CarrinhoPage from './Routes/Carrinho'; 
+import EstoquePage from './Routes/Estoque';   
+import LoginPage from './Routes/Login';     
+import ErrorPage from './Routes/Error';     
 
 function App() {
-
-
   return (
-    <>
-    
-    </>
-  )
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+
+        <main className="flex-grow container mx-auto p-4">
+          <Routes>
+            <Route path="/" element={<CarrinhoPage />} />
+            <Route path="/carrinhos" element={<CarrinhoPage />} />
+            <Route path="/estoque" element={<EstoquePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="*" element={<ErrorPage />} /> 
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
